925657a35891691e6336910c02bd19c56f3ea65b
Joshi-Tech <117322713+Joshi-Tech@users.noreply.github.com>
2024-11-26 11:35:21 +0000

    Update ReqResServices.java
    Rectified error

--- src/main/java/com/retail/cart/services/ReqResServices.java
+++ src/main/java/com/retail/cart/services/ReqResServices.java
@@ -16,7 +16,7 @@ import java.util.Map;
 public class ReqResServices {
 
     private static final String BASE_URL = "https://reqres.in";
-    private static final String GET_ALL_USERSS = "/api/users/";
+    private static final String GET_ALL_USERS = "/api/users/";
 
     @Autowired
     private ReqResGetPojo reqResGetPojo;
